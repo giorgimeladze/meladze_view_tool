a67d4a0a934a97ea08a659af425dc16f229f29f8
Giorgi Meladze <giorgi.meladze711@gmail.com>
2020-03-07 23:07:56 +0400

    Initial Commit

---
+++ lib/meladze_view_tool.rb
@@ -0,0 +1,7 @@
+require "meladze_view_tool/version"
+require "meladze_view_tool/renderer"
+
+module MeladzeViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/meladze_view_tool/version.rb
@@ -0,0 +1,3 @@
+module MeladzeViewTool
+  VERSION = "0.1.0"
+end
